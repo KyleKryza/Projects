2da74ab3b3c9c31f034a90de2e51189f1f7fe1ee
KyleKryza <ximkyx@gmail.com>
2023-08-01 18:11:41 -0700

    rect

--- PyGameIntro/runner.py
+++ PyGameIntro/runner.py
@@ -16,9 +16,15 @@ clock = pygame.time.Clock()
 
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/Sky.png")
-ground_surface = pygame.image.load("graphics/Ground.png")
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/Ground.png").convert()
 text_surface = test_font.render("Hello World!", False, "Black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
+player_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -30,6 +36,16 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300,50))
+    
+
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+    snail_rect.left -= 4
+    if snail_rect.left < -100:
+        snail_rect.left = 850
+
+    if player_rect.colliderect(snail_rect):
+        print("Collide")
 
     #update everything
     pygame.display.update()
